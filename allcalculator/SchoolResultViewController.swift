75a4ba4623482b5354270d13c9d9761400169ea1
NeelMoradiya9 <131774687+NeelMoradiya9@users.noreply.github.com>
2023-11-10 10:49:29 +0530

    abc

---
+++ allcalculator/SchoolResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SchoolResultViewController.swift
+//  allcalculator
+//
+//  Created by Neel  on 09/11/23.
+//
+
+import UIKit
+
+class SchoolResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ allcalculator/emiviewcon.swift
@@ -0,0 +1,24 @@
+//
+//  EMIViewController.swift
+//  allcalculator
+//
+//  Created by Neel  on 09/11/23.
+//
+
+import UIKit
+
+class emiviewcon: UIViewController {
+    @IBOutlet weak var loanamounttext: UITextField!
+    @IBOutlet weak var intrestratetext: UITextField!
+    @IBOutlet weak var timetext: UITextField!
+    @IBOutlet weak var intrerestamounttext: UILabel!
+    @IBOutlet weak var totalinteresttext: UILabel!
+    @IBOutlet weak var totalpayableamounttext: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func calculatebtn(_ sender: Any) {
+    }
+}
